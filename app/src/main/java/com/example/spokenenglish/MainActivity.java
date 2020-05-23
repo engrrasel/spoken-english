67f65e05f7223e57a65b1b4daa3a0dcf4c33e0e2
Rasel <eng.raselmiah@gmail.com>
2020-05-23 09:34:00 +0600

    marquee text added..

--- app/src/main/java/com/example/spokenenglish/MainActivity.java
+++ app/src/main/java/com/example/spokenenglish/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private CardView cardView;
+    Window window;
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +27,18 @@ public class MainActivity extends AppCompatActivity {
                 Intent intent= new Intent(MainActivity.this,spoken.class);
                 startActivity(intent);
 
+
+
             }
         });
+
+        if (Build.VERSION.SDK_INT>=21){
+            window=this.getWindow();
+            window.setStatusBarColor(this.getResources().getColor(R.color.black));
+            textView=findViewById(R.id.marquee);
+
+            textView.setSelected(true);
+        }
+
     }
 }
