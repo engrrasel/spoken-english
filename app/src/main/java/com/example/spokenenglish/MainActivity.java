e82f6c264fb737bece1ac76743b63e8400a8380b
Rasel <eng.raselmiah@gmail.com>
2020-06-15 13:39:23 +0600

    viewpager adepter completed.

--- app/src/main/java/com/example/spokenenglish/MainActivity.java
+++ app/src/main/java/com/example/spokenenglish/MainActivity.java
@@ -12,9 +12,9 @@ import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    public
-    /*CardView spokenenglish,tense;*/
-            Button spokenenglish,tense,learneveryday,verbs,proverbs,groupverb,phrase_idiomsid,about;
+
+
+    public CardView spokenenglish, tense, learneveryday, verbs, proverbs, phrase_idiomsid, about;
 
     Window window;
     TextView textView;
@@ -24,29 +24,30 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        spokenenglish=findViewById(R.id.spokenenglishid);
-        tense=findViewById(R.id.tenseid);
-        learneveryday=findViewById(R.id.learneverydayid);\
-        verbs=findViewById(R.id.verbsid);
-        proverbs=findViewById(R.id.proverbsid);
-        groupverb=findViewById(R.id.groupverbid);
-        phrase_idiomsid=findViewById(R.id.phrase_idiomsid);
-        about=findViewById(R.id.aboutid);
-
-
-
+        spokenenglish = (CardView) findViewById(R.id.spokenenglishid);
+        tense = (CardView) findViewById(R.id.tenseid);
+        learneveryday = (CardView) findViewById(R.id.learn_everydayid);
+        verbs = (CardView) findViewById(R.id.verbsid);
+        proverbs = (CardView) findViewById(R.id.proverbsid);
+        phrase_idiomsid = (CardView) findViewById(R.id.phrase_idiomsid);
+        about = (CardView) findViewById(R.id.aboutid);
 
 
         spokenenglish.setOnClickListener(this);
         tense.setOnClickListener(this);
+        learneveryday.setOnClickListener(this);
+        verbs.setOnClickListener(this);
+        proverbs.setOnClickListener(this);
+        phrase_idiomsid.setOnClickListener(this);
+        about.setOnClickListener(this);
 
-        if (Build.VERSION.SDK_INT>=21){
-            window=this.getWindow();
+
+        if (Build.VERSION.SDK_INT >= 21) {
+            window = this.getWindow();
             window.setStatusBarColor(this.getResources().getColor(R.color.ash));
 
 
-            textView=findViewById(R.id.marquee);
+            textView = findViewById(R.id.marquee);
 
             textView.setSelected(true);
         }
@@ -57,21 +58,45 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     public void onClick(View v) {
 
         Intent cardview;
-        switch (v.getId()){
-            case R.id.spokenenglishid:cardview=new Intent(this,spoken.class);
-            startActivity(cardview);
-            break;
-
-            case R.id.tenseid:cardview=new Intent(this,tense_activity.class);
-                startActivity(cardview);
-            break;
-
-            case R.id.learneverydayid:cardview=new Intent(this,learn_everyday.class);
+        switch (v.getId()) {
+            case R.id.spokenenglishid:
+                cardview = new Intent(this, spoken.class);
                 startActivity(cardview);
                 break;
 
-            case R.id.verbsid:cardview= new Intent(MainActivity.this,)
+            case R.id.tenseid:
+                cardview = new Intent(this, tense_activity.class);
+                startActivity(cardview);
+                break;
+
+            case R.id.learn_everydayid:
+                cardview = new Intent(this, learn_everyday.class);
+                startActivity(cardview);
+                break;
+
+            case R.id.verbsid:
+                cardview = new Intent(MainActivity.this, verbs_activity.class);
+                startActivity(cardview);
+                break;
+
+            case R.id.proverbsid:
+                cardview = new Intent(MainActivity.this, proverb_activity.class);
+                startActivity(cardview);
+                break;
+
+            case R.id.phrase_idiomsid:
+                cardview = new Intent(MainActivity.this, phrase_idioms_activity.class);
+                startActivity(cardview);
+                break;
+
+            case R.id.aboutid:
+                cardview = new Intent(MainActivity.this, about_activity.class);
+                startActivity(cardview);
+                break;
+
+
         }
 
     }
 }
+
