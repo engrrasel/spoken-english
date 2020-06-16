af19db94f996199f0fe6f333ad855db450f87056
Rasel <eng.raselmiah@gmail.com>
2020-06-16 23:16:43 +0600

    phrases and idioms added

--- app/src/main/java/com/example/spokenenglish/groupverb_activity.java
+++ app/src/main/java/com/example/spokenenglish/groupverb_activity.java
@@ -3,12 +3,27 @@ package com.example.spokenenglish;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class groupverb_activity extends AppCompatActivity {
+
+    private ListView groupverblist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_groupverb_activity);
+
+
+
+        groupverblist=(ListView) findViewById(R.id.groupverb_listeid);
+
+
+        String[] groupverbstring=getResources().getStringArray(R.array.phrase_data);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,R.layout.activity_groupverb_activity,R.id.groupverb_listeid,groupverbstring);
+        groupverblist.setAdapter(adapter);
+
     }
 }
--- app/src/main/java/com/example/spokenenglish/phrase_idioms_activity.java
+++ app/src/main/java/com/example/spokenenglish/phrase_idioms_activity.java
@@ -3,12 +3,25 @@ package com.example.spokenenglish;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class phrase_idioms_activity extends AppCompatActivity {
+
+    private ListView phraselistview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_phrase_idioms_activity);
+
+        phraselistview=(ListView) findViewById(R.id.phraselistviewid);
+
+        String[] phrase_idioms = getResources().getStringArray(R.array.phrase_data);
+
+        ArrayAdapter<String> adapter=new ArrayAdapter<>(this,R.layout.phrase_layout_desigine,R.id.phrasedesigineid,phrase_idioms);
+        phraselistview.setAdapter(adapter);
+
+
     }
 }
